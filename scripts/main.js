4a30f6972577d8aa651f0dcb62c6181c779972e0
whoisamellie <amelie.baimukanova@gmail.com>
2025-03-06 17:14:47 +0500

    second commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+import Header from "./Header.js";
+import TabsCollection from "./Tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
+import InputMaskCollection from './InputMask.js'
+import SelectCollection from "./Select.js";
+
+new Header ()
+new TabsCollection ()
+new VideoPlayerCollection ()
+new ExpandableContentCollection()
+new InputMaskCollection()
+new SelectCollection()
